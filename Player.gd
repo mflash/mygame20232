525fc14da3232643209b30712f793a62634148d1
Marcelo Cohen <mflashbr@gmail.com>
2023-09-18 19:02:11 -0300

    Movimento lateral com saltos e física

--- Player.gd
+++ Player.gd
@@ -2,8 +2,11 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (float) var rotation_speed = 2.5
+export (int) var gravity = 2500
+export (int) var jump_speed = -1000 
 
-onready var target = position
+onready var target := position
+onready var sprite := $Sprite
 
 var velocity = Vector2.ZERO
 var rotation_dir = 0
@@ -16,6 +19,19 @@ func get_8way_input():
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = velocity.normalized() * speed
+	
+	if velocity.y > 0:
+		sprite.play("down")
+	elif velocity.y < 0:
+		sprite.play("up")
+	elif velocity.x > 0:
+		sprite.play("right")
+	elif velocity.x < 0:
+		sprite.play("left")	
+	else:
+		sprite.stop()
+		sprite.frame = 0
+		
 	print(velocity)
 	
 func get_rotation_input():
@@ -39,6 +55,24 @@ func get_mouse_input():
 	if Input.is_action_pressed("up"):
 		velocity = Vector2(speed, 0).rotated(rotation)
 
+func get_side_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('jump')
+
+	if jump and is_on_floor():		
+		velocity.y = jump_speed
+	if right:
+		velocity.x += speed
+		sprite.play("right")
+	elif left:
+		velocity.x -= speed
+		sprite.play("left")	
+	else:
+		sprite.frame = 0
+		sprite.stop()
+		
 func _physics_process(delta):
 	#get_8way_input() # 1.
 	#get_rotation_input() # 2.
@@ -47,7 +81,13 @@ func _physics_process(delta):
 	#velocity = move_and_slide(velocity) # TODOS menos o 4
 	
 	# Point and click # 4.
-	velocity = position.direction_to(target) * speed
+	#velocity = position.direction_to(target) * speed
 	#look_at(target)
-	if position.distance_to(target) > 5:
-		velocity = move_and_slide(velocity)
+	#if position.distance_to(target) > 5:
+	#	velocity =  move_and_slide(velocity)
+	get_side_input()
+	velocity.y += gravity * delta
+	print(velocity)	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	#velocity =  move_and_slide(velocity) # 1.
+	#move_and_collide(velocity * delta)
